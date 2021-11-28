5ddcbb847c3ebe2e1b5e5a2b72eb0be3085ebaca
Nick Latham <nlatham@zagmail.gonzaga.edu>
2021-11-27 16:33:26 -0800

    stuff

--- myCtrl.js
+++ myCtrl.js
@@ -1,4 +1,25 @@
-app.controller("myCtrl", function($scope){
+app.controller("myCtrl", function($scope, $http){
     $scope.text = "Hello World";
     $scope.myCol = "lightblue";
+
+    $scope.names = [
+        {name:'Jani',country:'Norway'},
+        {name:'Carl',country:'Sweden'},
+        {name:'Margareth',country:'England'},
+        {name:'Hege',country:'Norway'},
+        {name:'Joe',country:'Denmark'},
+        {name:'Gustav',country:'Sweden'},
+        {name:'Birgit',country:'Denmark'},
+        {name:'Mary',country:'England'},
+        {name:'Kai',country:'Norway'}
+    ];
+
+    $scope.orderByMe = function(x) {
+        $scope.myOrderBy = x;
+    }
+
+    $http.get("welcome.htm")
+    .then(function(response) {
+      $scope.myWelcome = response.data;
+    });
 });
